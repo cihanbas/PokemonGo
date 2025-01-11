d3ce66cb6acbc59744664f331ef23bfd4c3cf38a
cihan baş <chnbs@hotmail.com>
2025-01-11 15:14:01 +0300

    feat: added Provider and PersistGate to app.tsx

--- App.tsx
+++ App.tsx
@@ -1,7 +1,16 @@
 import React from 'react';
 import MainNavigation from './src/navigation';
+import {Provider} from 'react-redux';
+import {persistor, store} from '@store';
+import {PersistGate} from 'redux-persist/integration/react';
 const App = () => {
-  return <MainNavigation />;
+  return (
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <MainNavigation />
+      </PersistGate>
+    </Provider>
+  );
 };
 
 export default App;
