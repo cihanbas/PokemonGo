8a3c5f757323cb85ff259a641bf8640fc200646f
cihan baş <chnbs@hotmail.com>
2025-01-11 14:30:40 +0300

    chore: absolute import configuration set up

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,25 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        alias: {
+          '@utils': './src/utils',
+          '@screens': './src/screens',
+          '@components': './src/components',
+          '@hooks': './src/hooks',
+          '@store': './src/store',
+          '@models': './src/models',
+          '@services': './src/services',
+        },
+      },
+    ],
+  ],
+  env: {
+    production: {
+      plugins: ['transform-remove-console'], //removing consoles.log from app during release (production) versions
+    },
+  },
 };
